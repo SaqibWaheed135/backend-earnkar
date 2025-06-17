131a5ed786e220ec2b1184f7ecce59231f7d38b3
SaqibWaheed135 <xaqibwaheed333@gmail.com>
2025-06-17 13:36:25 +0500

    Added routes of Ads

--- api/index.js
+++ api/index.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+
 // Routes
 app.use('/api/auth', require('../routes/authRoutes'));
 
